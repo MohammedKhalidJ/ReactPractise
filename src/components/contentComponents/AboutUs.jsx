a379e57a4e0e07adf88f936a97e85118b2570712
MohammedKhalidJ <mohammedkhalid11@gmail.com>
2018-12-08 18:51:52 +0530

    home page changes
    Null checking for home page

--- src/components/contentComponents/AboutUs.jsx
+++ src/components/contentComponents/AboutUs.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Panel } from 'react-bootstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Panel } from "react-bootstrap";
 
-const AboutUs = (props) => {
-    console.log(props);
-    return (
-        <React.Fragment>
-            <Panel.Body>
-                {props.name}
-                <ul>
-                    {props.list.map((v, i) => <li key={i.toString()}>{v}</li>)}
-                </ul>
-            </Panel.Body>
-        </React.Fragment>
-    );
-}
+const AboutUs = props => {
+  console.log(props);
+  return (
+    <React.Fragment>
+      <Panel.Body>
+        {props.name || null}
+        <ul>
+          {props.list.map((v, i) => (
+            <li key={i.toString()}>{v}</li>
+          ))}
+        </ul>
+      </Panel.Body>
+    </React.Fragment>
+  );
+};
 
 AboutUs.propTypes = {
-    name: PropTypes.string,
-    list: PropTypes.array
+  name: PropTypes.string,
+  list: PropTypes.array
 };
 
 AboutUs.defaultProps = {
-    list: [1,2,3]
+  list: [1, 2, 3]
 };
 
 export default AboutUs;
